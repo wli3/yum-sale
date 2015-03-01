a888ac15128eda4be5641c3969385fe61c1efcc5
Wu Li <wli3@wpi.edu>
2015-03-01 16:06:22 -0500

    initial scaffolding

---
+++ YumSale/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(YumSale.Startup))]
+namespace YumSale
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
