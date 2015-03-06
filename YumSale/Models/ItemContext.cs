553004eef798ed36a113b6ec4e477c92b6566df1
Wu Li <wli3@wpi.edu>
2015-03-05 22:39:58 -0500

    remove not working code

--- YumSale/Models/ItemContext.cs
+++ YumSale/Models/ItemContext.cs
@@ -8,74 +8,11 @@ namespace YumSale.Models
 {
     public class ItemContext : DbContext
     {
+        public ItemContext() : base("DefaultConnection")
+        {
+        }
+
         public DbSet<Item> Items { get; set; }
         public DbSet<Buyer> Buyers { get; set; }
-
-
-        public void Seed(ItemContext context)
-        {
-#if DEBUG
-            // Create my debug (testing) objects here
-            var item1 = new Item()
-            {
-                ItemId = 1,
-                Name = "item1",
-                CreateDateTime = new DateTime(2015, 1, 1),
-                Descrption = "good item1",
-                HoldLong = new TimeSpan(1)
-            };
-
-            var item2 = new Item()
-            {
-                ItemId = 1,
-                Name = "item2",
-                CreateDateTime = new DateTime(2015, 1, 2),
-                Descrption = "good item2",
-                HoldLong = new TimeSpan(2)
-            };
-
-            var item3 = new Item()
-            {
-                ItemId = 1,
-                Name = "item3",
-                CreateDateTime = new DateTime(2015, 1, 3),
-                Descrption = "good item3",
-                HoldLong = new TimeSpan(3)
-            };
-            context.Items.Add(item1);
-            context.Items.Add(item2);
-            context.Items.Add(item3);
-#endif
-            context.SaveChanges();
-        }
-
-        public class DropCreateIfChangeInitializer : DropCreateDatabaseIfModelChanges<ItemContext>
-        {
-            protected override void Seed(ItemContext context)
-            {
-                context.Seed(context);
-
-                base.Seed(context);
-            }
-        }
-
-        public class CreateInitializer : CreateDatabaseIfNotExists<ItemContext>
-        {
-            protected override void Seed(ItemContext context)
-            {
-                context.Seed(context);
-
-                base.Seed(context);
-            }
-        }
-
-        static ItemContext()
-        {
-            #if DEBUG
-            Database.SetInitializer<ItemContext> (new DropCreateIfChangeInitializer ());
-            #else
-            Database.SetInitializer<ItemContext> (new CreateInitializer ());
-            #endif
-        }
     }
 }
