109b2e5f9f5678172446bf6449052315dc45bed6
William Lee <wli3@wpi.edu>
2015-04-24 21:22:13 -0400

    RichClient/Items basic template

--- YumSale/Areas/HelpPage/App_Start/HelpPageConfig.cs
+++ YumSale/Areas/HelpPage/App_Start/HelpPageConfig.cs
@@ -33,7 +33,7 @@ namespace YumSale.Areas.HelpPage
             Justification = "Part of a URI.")]
         public static void Register(HttpConfiguration config)
         {
-            config.SetDocumentationProvider(new XmlDocumentationProvider(HttpContext.Current.Server.MapPath("~/App_Data/XmlDocument.xml")));
+            config.SetDocumentationProvider(new XmlDocumentationProvider(HttpContext.Current.Server.MapPath("~/bin/XmlDocument.xml")));
 
             //// Uncomment the following to use "sample string" as the sample for all actions that have string as the body parameter or return type.
             //// Also, the string arrays will be used for IEnumerable<string>. The sample objects will be serialized into different media type 
---
+++ YumSale/Controllers/RichClientController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace YumSale.Controllers
+{
+    public class RichClientController : Controller
+    {
+        public ActionResult Items(string id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.userId = id;
+            return View();
+        }
+    }
+}
