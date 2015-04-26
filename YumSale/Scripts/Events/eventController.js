0ab9311859d74977468a25dad0336ffccb0fa1e9
William Lee <wli3@wpi.edu>
2015-04-25 22:13:55 -0400

    rich client have better ui, button like display

--- YumSale/Scripts/Events/eventController.js
+++ YumSale/Scripts/Events/eventController.js
@@ -2,7 +2,18 @@
     var baseLink = $("#baseLink").data("value");
     var userId = $("#userId").data("value");
     var apiLink = "http://" + baseLink + "/api/buy/items/" + userId;
-    // $http.get("http://localhost:49883/api/buy/items/74e8220e-35e1-440e-8308-71820009916c")
     $http.get(apiLink)
-        .success(function(response) { $scope.items = response; });
+        .success(function (response) { $scope.items = response; });
+    //0:  {
+    //    itemId: 2
+    //    name: "coffee bean"
+    //    descrption: "good coffee bean"
+    //    imageUrl: "http://www.support4change.com/blog/wp-content/uploads/2012/04/coffee-bean.jpg"
+    //    holdLongLessThanDay: "01:00:00"
+    //    holdLongDay: 3
+    //    buyerName: null
+    //    contact: null
+    //    token: null
+    //    holdTime: null
+    //}
 });
